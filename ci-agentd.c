a7bf06be1e60e97a30b46ba5ca12d873cfe85579
David Hernandez <david.hernandez@tobii.com>
2026-02-25 19:56:31 +0100

    Added a defensive check in get_mono_ms() so a clock_gettime failure returns 0 instead of using uninitialized data

--- ci-agentd.c
+++ ci-agentd.c
@@ -592,7 +592,8 @@ static unsigned long long get_mono_ms(void)
 {
 	struct timespec ts;
 
-	clock_gettime(CLOCK_MONOTONIC, &ts);
+	if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0)
+		return 0;
 	return (unsigned long long)ts.tv_sec * 1000ULL +
 	       (unsigned long long)ts.tv_nsec / 1000000ULL;
 }
